1f077f6fba4d563fb2721c5ac0edea4ce7c6c6c5
Laniakea <Laniakea@git.com>
2022-05-04 14:46:52 +0800

    First commit

---
+++ src/main/java/org/git/GitTest.java
@@ -0,0 +1,12 @@
+package org.git;
+
+/**
+ * @Description TODO
+ * @Author 0X27
+ * @Date 2022年5月4日 下午2:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,git");
+    }
+}
